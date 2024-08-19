0a00cb75da1ec0d3777d71b3206c65fc9831884a
Boubacar <boubacar@MBPdeIsgaDiallo.lan>
2024-08-18 22:26:01 -0700

    added pages to app, created header component, footer component and body

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,41 @@
+import './Footer.scss'
+import ProfilePic from '../../assets/images/Profile.jpeg'
+import {Container, Row, Col, Stack, Image, Nav, NavLink} from "react-bootstrap"
+
+const Footer = () => {
+    return (
+        <footer className='fixed-bottom'>
+            <Container fluid>
+                <Row className='bg-primary text-white'>
+                    <Col className="max-5">
+                    <Stack>
+                        <Image className='image'
+                            src={ProfilePic}
+                            alt="profilePic"
+                            rounded
+                            width={100}
+                            height={150}
+                        />
+                    </Stack>
+                    </Col>
+                    <Col>
+                        <Nav className='flex-column fs-5'>
+                            Useful Links
+                            <NavLink href='#' className='text-white'>LinkedIn</NavLink>
+                            <NavLink href='#' className='text-white'>Github</NavLink>
+                        </Nav>
+                    </Col>
+                    <Col>
+                        <h4>Contact me!</h4>
+                        <p>bdiallo10@icloud.com</p>
+                        <p>diallo.hhrd@gmail.com</p>
+                        <p>Phone: 206-476-0632</p>
+                    </Col>
+                </Row>
+            </Container>
+        </footer>
+    )
+    
+}
+
+export default Footer
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,22 @@
 import './Header.scss'
+import ProfilePic from '../../assets/images/Profile.jpeg'
 
 const Header = () => {
 
   return (
-    <div>
-        <h1>Welcome to my page</h1>
+    <div className='header'>
+        <img src={ProfilePic} alt="Profile Pics" />
+        <h1>
+        Hello, my name is Boubacar Diallo. 
+        I am a highly-motivated employee with desire to 
+        take on a new challenges. My strenghts are work ethic, 
+        adaptability, interpersonal skills, critical thinking 
+        and always willing to learn new technologies.
+        </h1>
+        <h1>
+        I hold professional certification in Front End Web Development,
+        Full Stack Web Development, Applied Data Science with Python.
+        </h1>
     </div>
   )
 }
